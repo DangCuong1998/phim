40709de46f032f0f4b45f347213a75354b19e7c0
cuongdang <cuongdq62@wru.vn>
2022-02-22 22:36:22 +0700

    day len

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<title>Ten Phim Demo ( Phan xxx ) Tap zzz</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<meta name="_token" id="token" value="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="cache-control" content="no-cache" />
+	<meta name="robots" content="index, follow" />
+	<meta name="language" content="Vietnamese, English" />
+	<meta name="theme-color" content="#d80f16">
+	<link rel="shortcut icon" href="https://phimnhanh1.com/favicon.ico" type="image/x-icon" />
+	<meta name="description" content="Ten Phim Demo ( Phan xxx ) Tap zzz" />
+	<meta name="keywords" content="Ten Phim Demo phan xxx,Ten Phim Demo phan 3 tap zzz" />
+
+	<meta property="fb:app_id" content="249309905703466" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="http://phimnhanh1.com/tuyet-ung-linh-chu-phan-3/tap-1" />
+	<meta property="og:description" content="Ten Phim Demo ( Phan xxx ) Tap 1" />
+	<meta property="og:title" content="Ten Phim Demo ( Phan xxx ) Tap 1" />
+	<meta property="og:site_name" content="Xem Phim Nhanh Nhất" />
+	<meta property="og:type" content="video.movie" />
+
+	<meta itemprop="name" content="Ten Phim Demo ( Phan xxx ) Tap 1" />
+	<meta itemprop="description" content="Ten Phim Demo ( Phan xxx ) Tap 1" />
+	<meta itemprop="image" content="" />
+	<link rel="stylesheet" href="/css/film.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar">
+			<div class="navbar-container">
+				<div class="navbar-header">
+					<div class="navbar-brand">
+						<a class="logo" href="/">
+							PHIM NHANH 1
+						</a>
+					</div>
+					<div class="navbar-menu-toggle" id="navbar-toggle">
+						<i class="icon-menu"></i>
+					</div>
+				</div>
+				<div class="navbar-left" id="navbar-left">
+					<div class="navbar-search">
+						<div class="search-box">
+							<input type="text" name="search-box" placeholder="Tìm kiếm" autocomplete="off">
+							<i class="icon icon-search"></i>
+						</div>
+						<div class="search-result" id="search-result">
+							<div class="result-body"></div>
+							<div class="result-noitem hidden"></div>
+							<div class="loading hidden"></div>
+						</div>
+					</div>
+					<div class="navbar-menu">
+						<li class="navbar-menu-item"><a href="/phim-chieu-rap.html"><i class="icon icon-ribbon"></i> Phim Chiếu Rạp</a></li>
+						<li class="navbar-menu-item"><a href="/phim-le.html"><i class="icon icon-film"></i> Phim Lẻ</a></li>
+						<li class="navbar-menu-item"><a href="/phim-bo.html"><i class="icon icon-news"></i> Phim Bộ</a></li>
+					</div>
+					<div class="navbar-close">
+						<i class="icon-close"></i>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<div class="container" data-id="20587" data-episode-id="286996" data-episode-name="1" data-type="" data-is-upcoming="" data-copyrighted="" data-quality="2" data-audio="0" data-name="Ten Phim Demo (Phần 3)">
+		<div id="sponsor-header" class="banner-masthead hidden"></div>
+		<div class="player-wrapper  drama ">
+			<iframe style="width: 100%" height="315"
+			src="https://www.youtube.com/embed/VrgGi7Va33Y">
+			</iframe>
+		</div>
+		<div id="sponsor-under" class="hidden"></div>
+		<div class="related-block">
+			<div id="sponsor-right" class="ff-banner hidden"></div>
+			<div class="related-item">
+				<a href="/detail.html" target="_blank">
+					<img class="related-item-thumbnail" src="https://picsum.photos/490/276" alt="Demo title">
+				</a>
+				<div class="related-item-meta">
+					<a href="/detail.html" target="_blank">
+						<div class="related-item-title">Demo title</div>
+					</a>
+					<span class="related-item-views">2 giờ trước</span>
+				</div>
+			</div>
+			
+			<div class="related-item">
+				<a href="/detail.html" target="_blank">
+					<img class="related-item-thumbnail" src="https://picsum.photos/490/276" alt="Demo title">
+				</a>
+				<div class="related-item-meta">
+					<a href="/detail.html" target="_blank">
+						<div class="related-item-title">Demo title</div>
+					</a>
+					<span class="related-item-views">2 giờ trước</span>
+				</div>
+			</div>
+			<div class="related-item">
+				<a href="/detail.html" target="_blank">
+					<img class="related-item-thumbnail" src="https://picsum.photos/490/276" alt="Demo title">
+				</a>
+				<div class="related-item-meta">
+					<a href="/detail.html" target="_blank">
+						<div class="related-item-title">Demo title</div>
+					</a>
+					<span class="related-item-views">2 giờ trước</span>
+				</div>
+			</div>
+			<div class="related-item">
+				<a href="/detail.html" target="_blank">
+					<img class="related-item-thumbnail" src="https://picsum.photos/490/276" alt="Demo title">
+				</a>
+				<div class="related-item-meta">
+					<a href="/detail.html" target="_blank">
+						<div class="related-item-title">Demo title</div>
+					</a>
+					<span class="related-item-views">2 giờ trước</span>
+				</div>
+			</div>
+		</div>
+		<div class="film-info">
+			<div id="sponsor-bottom" class="hidden"></div>
+			<div class="film-info-header">
+				<h1 class="film-info-title">Ten Phim Demo ( Phan xxx ) Tap 1</h1>
+				<div class="film-info-views">Ten phim/ tac gia / Lord Xue Ying</div>
+				<div class="film-info-views">
+					<span>Lượt xem: 99999999999999999999 </span>
+				</div>
+			</div>
+			<div class="film-content">
+				<div class="film-info-genre">
+					Cập nhật:
+					10 / 26 tập
+				</div>
+				<div class="film-episode">
+					<a class="episode-item episode-10" href="/tuyet-ung-linh-chu-phan-3/tap-10"><span>Tập </span>10</a>
+					<a class="episode-item episode-9" href="/tuyet-ung-linh-chu-phan-3/tap-9"><span>Tập </span>9</a>
+					<a class="episode-item episode-8" href="/tuyet-ung-linh-chu-phan-3/tap-8"><span>Tập </span>8</a>
+					<a class="episode-item episode-7" href="/tuyet-ung-linh-chu-phan-3/tap-7"><span>Tập </span>7</a>
+					<a class="episode-item episode-6" href="/tuyet-ung-linh-chu-phan-3/tap-6"><span>Tập </span>6</a>
+					<a class="episode-item episode-5" href="/tuyet-ung-linh-chu-phan-3/tap-5"><span>Tập </span>5</a>
+					<a class="episode-item episode-4" href="/tuyet-ung-linh-chu-phan-3/tap-4"><span>Tập </span>4</a>
+					<a class="episode-item episode-3" href="/tuyet-ung-linh-chu-phan-3/tap-3"><span>Tập </span>3</a>
+					<a class="episode-item episode-2" href="/tuyet-ung-linh-chu-phan-3/tap-2"><span>Tập </span>2</a>
+					<a class="episode-item episode-1" href="/tuyet-ung-linh-chu-phan-3/tap-1"><span>Tập </span>1</a>
+				</div>
+				<div class="film-info-description">
+					description
+				</div>
+				<div class="film-info-tag">
+					<i class="icon-tags"></i>
+					<a href="/detail.html" target="_blank">Ten Phim Demo</a> </div>
+					<div class="film-info-tag">
+						<span>Ten Phim Demo (Phần 3)</span><span>Xue Ying Ling Zhu 3rd Season / Lord Xue Ying</span><span>phim Ten Phim Demo (Phần 3)</span>
+						<span>Ten Phim Demo ( Phan xxx ) Tap 1</span> <span>Ten Phim Demo ( Phan xxx ) Tap 10</span><span>Ten Phim Demo (Phần 3) mới nhất</span>
+						<span>Ten Phim Demo (Phần 3) Vietsub</span>
+						<span>Ten Phim Demo (Phần 3) HD</span>
+					</div>
+				</div>
+			</div>
+
+			<section class="tray episode">
+				<div class="tray-title">
+					<span class="icon icon-cinema"></span><h5><a href="/tap-moi-nhat.html">Mới Cập Nhật <i class="icon icon-right"></i></a></h5>
+				</div>
+				<div class="tray-content index">
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+					<div class="tray-item">
+						<a href="/detail.html">
+							<img class="tray-item-thumbnail" src="https://picsum.photos/240/133" alt="Demo title">
+							<div class="tray-item-description">
+								<div class="tray-episode-name">Tap ??</div>
+								<div class="tray-item-title">Demo title</div>
+							</div>
+							<div class="tray-item-audio">
+								Vietsub
+							</div>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<div id="sponsor-catfish" class="hidden"></div>
+			<div id="sponsor-balloon" class="hidden"></div>
+			<div id="sponsor-preload" class="hidden"></div>
+		</div>
+	</body>
+	</html>
